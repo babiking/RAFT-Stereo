367c6c46b7eb90ec3dfc549b76a74891bacaaaba
babiking <brojackfeely@163.com>
2023-07-31 16:02:04 +0800

    use gflags instead of argparser

--- demo.py
+++ demo.py
@@ -1,31 +1,36 @@
+import os
 import sys
-sys.path.append('core')
-
 import argparse
 import glob
 import numpy as np
 import torch
 from tqdm import tqdm
 from pathlib import Path
-from raft_stereo import RAFTStereo
-from utils.utils import InputPadder
+from core.raft_stereo import RAFTStereo
+from core.utils.utils import InputPadder
 from PIL import Image
 from matplotlib import pyplot as plt
 
+import gflags
 
-DEVICE = 'cuda'
+gflags.DEFINE_string("device", "cuda", "pytorch inference device, cpu | cuda")
 
-def load_image(imfile):
+
+def load_image(imfile, device):
     img = np.array(Image.open(imfile)).astype(np.uint8)
     img = torch.from_numpy(img).permute(2, 0, 1).float()
-    return img[None].to(DEVICE)
+    return img[None].to(device)
+
 
 def demo(args):
+    FLAGS = gflags.FLAGS
+    FLAGS(sys.argv)
+
     model = torch.nn.DataParallel(RAFTStereo(args), device_ids=[0])
     model.load_state_dict(torch.load(args.restore_ckpt))
 
     model = model.module
-    model.to(DEVICE)
+    model.to(FLAGS.device)
     model.eval()
 
     output_directory = Path(args.output_directory)
@@ -36,9 +41,9 @@ def demo(args):
         right_images = sorted(glob.glob(args.right_imgs, recursive=True))
         print(f"Found {len(left_images)} images. Saving files to {output_directory}/")
 
-        for (imfile1, imfile2) in tqdm(list(zip(left_images, right_images))):
-            image1 = load_image(imfile1)
-            image2 = load_image(imfile2)
+        for imfile1, imfile2 in tqdm(list(zip(left_images, right_images))):
+            image1 = load_image(imfile1, FLAGS.device)
+            image2 = load_image(imfile2, FLAGS.device)
 
             padder = InputPadder(image1.shape, divis_by=32)
             image1, image2 = padder.pad(image1, image2)
@@ -46,33 +51,104 @@ def demo(args):
             _, flow_up = model(image1, image2, iters=args.valid_iters, test_mode=True)
             flow_up = padder.unpad(flow_up).squeeze()
 
-            file_stem = imfile1.split('/')[-2]
+            file_stem = imfile1.split("/")[-2]
             if args.save_numpy:
-                np.save(output_directory / f"{file_stem}.npy", flow_up.cpu().numpy().squeeze())
-            plt.imsave(output_directory / f"{file_stem}.png", -flow_up.cpu().numpy().squeeze(), cmap='jet')
+                np.save(
+                    output_directory / f"{file_stem}.npy",
+                    flow_up.cpu().numpy().squeeze(),
+                )
+            plt.imsave(
+                output_directory / f"{file_stem}.png",
+                -flow_up.cpu().numpy().squeeze(),
+                cmap="jet",
+            )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--restore_ckpt', help="restore checkpoint", required=True)
-    parser.add_argument('--save_numpy', action='store_true', help='save output as numpy arrays')
-    parser.add_argument('-l', '--left_imgs', help="path to all first (left) frames", default="datasets/Middlebury/MiddEval3/testH/*/im0.png")
-    parser.add_argument('-r', '--right_imgs', help="path to all second (right) frames", default="datasets/Middlebury/MiddEval3/testH/*/im1.png")
-    parser.add_argument('--output_directory', help="directory to save output", default="demo_output")
-    parser.add_argument('--mixed_precision', action='store_true', help='use mixed precision')
-    parser.add_argument('--valid_iters', type=int, default=32, help='number of flow-field updates during forward pass')
+    parser.add_argument(
+        "--restore_ckpt",
+        help="restore checkpoint",
+        default="models/raftstereo-middlebury.pth",
+    )
+    parser.add_argument(
+        "--save_numpy", action="store_true", help="save output as numpy arrays"
+    )
+    parser.add_argument(
+        "-l",
+        "--left_imgs",
+        help="path to all first (left) frames",
+        default="armnn/test/data/left_0000.png",
+    )
+    parser.add_argument(
+        "-r",
+        "--right_imgs",
+        help="path to all second (right) frames",
+        default="armnn/test/data/right_0000.png",
+    )
+    parser.add_argument(
+        "--output_directory", help="directory to save output", default="demo_output"
+    )
+    parser.add_argument(
+        "--mixed_precision", action="store_true", help="use mixed precision"
+    )
+    parser.add_argument(
+        "--valid_iters",
+        type=int,
+        default=32,
+        help="number of flow-field updates during forward pass",
+    )
 
     # Architecture choices
-    parser.add_argument('--hidden_dims', nargs='+', type=int, default=[128]*3, help="hidden state and context dimensions")
-    parser.add_argument('--corr_implementation', choices=["reg", "alt", "reg_cuda", "alt_cuda"], default="reg", help="correlation volume implementation")
-    parser.add_argument('--shared_backbone', action='store_true', help="use a single backbone for the context and feature encoders")
-    parser.add_argument('--corr_levels', type=int, default=4, help="number of levels in the correlation pyramid")
-    parser.add_argument('--corr_radius', type=int, default=4, help="width of the correlation pyramid")
-    parser.add_argument('--n_downsample', type=int, default=2, help="resolution of the disparity field (1/2^K)")
-    parser.add_argument('--context_norm', type=str, default="batch", choices=['group', 'batch', 'instance', 'none'], help="normalization of context encoder")
-    parser.add_argument('--slow_fast_gru', action='store_true', help="iterate the low-res GRUs more frequently")
-    parser.add_argument('--n_gru_layers', type=int, default=3, help="number of hidden GRU levels")
-    
+    parser.add_argument(
+        "--hidden_dims",
+        nargs="+",
+        type=int,
+        default=[128] * 3,
+        help="hidden state and context dimensions",
+    )
+    parser.add_argument(
+        "--corr_implementation",
+        choices=["reg", "alt", "reg_cuda", "alt_cuda"],
+        default="reg",
+        help="correlation volume implementation",
+    )
+    parser.add_argument(
+        "--shared_backbone",
+        action="store_true",
+        help="use a single backbone for the context and feature encoders",
+    )
+    parser.add_argument(
+        "--corr_levels",
+        type=int,
+        default=4,
+        help="number of levels in the correlation pyramid",
+    )
+    parser.add_argument(
+        "--corr_radius", type=int, default=4, help="width of the correlation pyramid"
+    )
+    parser.add_argument(
+        "--n_downsample",
+        type=int,
+        default=2,
+        help="resolution of the disparity field (1/2^K)",
+    )
+    parser.add_argument(
+        "--context_norm",
+        type=str,
+        default="batch",
+        choices=["group", "batch", "instance", "none"],
+        help="normalization of context encoder",
+    )
+    parser.add_argument(
+        "--slow_fast_gru",
+        action="store_true",
+        help="iterate the low-res GRUs more frequently",
+    )
+    parser.add_argument(
+        "--n_gru_layers", type=int, default=3, help="number of hidden GRU levels"
+    )
+
     args = parser.parse_args()
 
     demo(args)
