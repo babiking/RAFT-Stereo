85219572880ca3c5b03dfd7e1f1d7ab447092b10
babiking <brojackfeely@163.com>
2023-09-13 17:25:44 +0800

    add tflite_model_inference

--- armnn/runtime.py
+++ armnn/runtime.py
@@ -2,23 +2,31 @@ import torch
 import cv2 as cv
 import numpy as np
 import onnxruntime
+import tensorflow as tf
 from core.raft_stereo import RAFTStereo
 from core.utils.utils import InputPadder
 
 
 def read_image_file(img_file):
-    # torch feature map dimension format: NCHW
-    img = cv.imread(img_file, cv.IMREAD_UNCHANGED)[np.newaxis, :, :, ::-1].astype(
-        np.float32
-    )
+    img = cv.imread(img_file, cv.IMREAD_UNCHANGED)
+
+    if (len(img.shape) < 3) or (len(img.shape) == 3 and img.shape[-1] == 1):
+        img = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
+
+    # color format BGR to RGB
+    img = img[:, :, ::-1]
+
+    img = img[np.newaxis, :, :, :]
+
+    # torch feature map dimension format: NCHW    
     img = np.transpose(img, [0, 3, 1, 2])
     return img
 
 
 def run_torch_model_inference(
-    pt_model_file,
-    l_img_file,
-    r_img_file,
+    l_img,
+    r_img,
+    torch_model_file,
     device="cuda",
     hidden_dims=[128, 128, 128],
     corr_implementation="reg",
@@ -33,14 +41,11 @@ def run_torch_model_inference(
     n_flow_updates=32,
     division=32,
 ):
-    l_img = read_image_file(l_img_file)
-    l_img = torch.tensor(l_img, dtype=torch.float32).to(device)
+    torch_l_img = torch.tensor(l_img, dtype=torch.float32).to(device)
+    torch_r_img = torch.tensor(r_img, dtype=torch.float32).to(device)
 
-    r_img = read_image_file(r_img_file)
-    r_img = torch.tensor(r_img, dtype=torch.float32).to(device)
-
-    padder = InputPadder(l_img.shape, divis_by=division)
-    l_img, r_img = padder.pad(l_img, r_img)
+    padder = InputPadder(torch_l_img.shape, divis_by=division)
+    torch_l_img, torch_r_img = padder.pad(torch_l_img, torch_r_img)
 
     model = torch.nn.DataParallel(
         RAFTStereo(
@@ -57,29 +62,50 @@ def run_torch_model_inference(
         ),
         device_ids=[0],
     )
-    model.load_state_dict(torch.load(pt_model_file))
+    model.load_state_dict(torch.load(torch_model_file))
 
     model = model.module
     model.to(device)
     model.eval()
 
-    _, l_disp = model(l_img, r_img, iters=n_flow_updates, test_mode=True)
-    l_disp = padder.unpad(l_disp).squeeze()
-    l_disp *= -1.0
+    _, torch_l_disp = model(torch_l_img, torch_r_img, iters=n_flow_updates, test_mode=True)
+    torch_l_disp = padder.unpad(torch_l_disp).squeeze()
+    torch_l_disp *= -1.0
+    return torch_l_disp.cpu().detach().numpy().squeeze()
 
-    return l_disp.cpu().detach().numpy().squeeze()
 
-
-def run_onnx_model_inference(onnx_model_file, l_img_file, r_img_file):
+def run_onnx_model_inference(l_img, r_img, onnx_model_file):
     session = onnxruntime.InferenceSession(onnx_model_file)
 
-    l_img = read_image_file(l_img_file)
-    r_img = read_image_file(r_img_file)
-
-    _, l_disp = session.run(
+    _, onnx_l_disp = session.run(
         None,
         {"left": l_img, "right": r_img},
     )
-    l_disp *= -1.0
+    onnx_l_disp *= -1.0
+    return np.squeeze(onnx_l_disp)
 
-    return np.squeeze(l_disp)
+
+def run_tflite_model_inference(l_img, r_img, tflite_model_file):
+    tflite_l_img = np.transpose(l_img, [0, 2, 3, 1])
+    tflite_r_img = np.transpose(r_img, [0, 2, 3, 1])
+
+    # Load the TFLite model and allocate tensors
+    interpreter = tf.lite.Interpreter(model_path=tflite_model_file)
+    interpreter.allocate_tensors()
+
+    # Get input and output tensors
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    interpreter.set_tensor(
+        input_details[0]["index"], tf.convert_to_tensor(tflite_l_img, dtype=tf.float32)
+    )
+    interpreter.set_tensor(
+        input_details[1]["index"], tf.convert_to_tensor(tflite_r_img, dtype=tf.float32)
+    )
+
+    interpreter.invoke()
+
+    tflite_l_disp = interpreter.get_tensor(output_details[1]["index"])
+    tflite_l_disp *= -1.0
+    return tflite_l_disp
